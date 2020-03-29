8ca3ffce6cad868d15c18273bc9fc34c70656206
suzuki ryosuke <suzuki@primestyle.co.jp>
2020-03-30 00:31:38 +0900

    Fix: correct axios proxy config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,11 +1,12 @@
 require("dotenv").config();
 module.exports = {
-    srcDir: "client/",
-    modules: ["@nuxtjs/axios", "@nuxtjs/proxy", "@nuxtjs/dotenv"],
-    axios: {
-        credentials: true
-    },
-    proxy: {
-        "/api": process.env.NUXT_ENV_PROXY_TARGET || "http://localhost:9000"
-    }
+  srcDir: "client/",
+  modules: ["@nuxtjs/axios", "@nuxtjs/proxy", "@nuxtjs/dotenv"],
+  axios: {
+    proxy: true,
+    credentials: true,
+  },
+  proxy: {
+    "/api": process.env.NUXT_ENV_PROXY_TARGET || "http://localhost:9000"
+  }
 };
